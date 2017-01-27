1a92ae5e28a211e42f19931e303dd08aad9a8e53
cleborys <clemens.borys@stud.uni-goettingen.de>
2017-01-27 12:18:05 +0100

    first exploration

--- clemme/doodle.py
+++ clemme/doodle.py
@@ -8,6 +8,7 @@ import pandas as pd
 import datetime
 import pprint #pretty printing
 import matplotlib.pyplot as plt
+import numpy as np
 
 def dt_to_wt(dt):
 ### datetime to weektime (linear from 0 to 7 with one week period. Monday 00:00 is zero)
@@ -45,6 +46,14 @@ try:
     plt.clf()
 
     daytimelist=df['committer_date_dyt'].tolist()
-
+    #square figure forced
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
+    N = 100
+    theta = np.arange(0.0, 2*np.pi, 2*np.pi/N)
+    values = [len([t for t in daytimelist if (n/N<= t <(n+1)/N)]) for n in range(0,N)]
+    width = 2*np.pi/N
+    bars = ax.bar(theta, values, width=width, bottom=0.0)
+    fig.savefig("dayperiod_polar.png")
 finally:
     client.close()
